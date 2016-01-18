a8cf407a7b0d9b19a295662f2b1a306098b21867
Helmut Hummel <info@helhum.io>
2016-01-18 16:05:59 +0100

    First working version

--- src/Plugin.php
+++ src/Plugin.php
@@ -16,7 +16,7 @@ use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\ScriptEvents;
-use TYPO3\CMS\Composer\Plugin\Util\Filesystem;
+use Composer\Util\Filesystem;
 
 /**
  * Class Plugin
@@ -68,14 +68,27 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      */
     public function onPostAutoloadDump(\Composer\Script\Event $event)
     {
-        $filesystem = new Filesystem();
         if (!file_exists($this->baseDir . '.env')) {
             return;
         }
-        $pathToRootCode = $filesystem->findShortestPathCode($this->composer->getConfig()->get('vendor-dir') . '/foo', $this->baseDir);
-        var_dump($pathToRootCode);
-        $code = "(new \\Dotenv\\Dotenv($pathToRootCode))->load();\n";
-        $this->insertCode($code);
+
+        if ($event->isDevMode()) {
+            $filesystem = new Filesystem();
+            $pathToRootCode = $filesystem->findShortestPathCode($this->composer->getConfig()->get('vendor-dir') . '/foo', $this->baseDir);
+            $envCode = <<<EOF
+\$dec = new \\Dotenv\\Dotenv($pathToRootCode);
+\$dec->overload();
+unset(\$dec);
+EOF;
+        } else {
+            $envCode = $this->getCachedCode($this->baseDir);
+        }
+        $code = <<<EOF
+// start insert by helhum/dotenv-connector
+$envCode
+// end by helhum/dotenv-connector
+EOF;
+        $this->insertCode("\n" . $code . "\n\n");
     }
 
     /**
@@ -89,17 +102,46 @@ class Plugin implements PluginInterface, EventSubscriberInterface
             throw new \RuntimeException(sprintf(
                 'Could not adjust autoloader: The file %s was not found.',
                 $autoloadFile
-            ));
+            ),
+            1453127591
+            );
         }
 
         $this->io->write('<info>Inserting dotenv initialization into main autoload file</info>');
 
+        $originalAutoloadFileContent = file_get_contents($autoloadFile);
+        preg_match('/\nreturn ([^;]+;\s*$)/mD', $originalAutoloadFileContent, $matches);
+        if (empty($matches[1])) {
+            throw new \RuntimeException(
+                'Could not adjust autoloader: autoload.php could not be parsed!',
+                1453127591
+            );
+        }
+        $code = $matches[1] . "\n" . $code;
         // Regex modifiers:
         // "m": \s matches newlines
         // "D": $ matches at EOF only
         // Translation: insert before the last "return" in the file
-        $contents = preg_replace('/\n(?=return [^;]+;\s*$)/mD', "\n" . $code, file_get_contents($autoloadFile));
+        $contents = preg_replace('/\n(?=return [^;]+;\s*$)/mD', "\n" . $code, $originalAutoloadFileContent);
         file_put_contents($autoloadFile, $contents);
     }
 
+    /**
+     * @param string $envFileDir
+     * @return string
+     */
+    protected function getCachedCode($envFileDir)
+    {
+        $superGlobalEnvBackup = $_ENV;
+        $dotEnv = new \Dotenv\Dotenv($envFileDir);
+        $dotEnv->overload();
+        $writtenEnvVars = array_diff_assoc($_ENV, $superGlobalEnvBackup);
+        $cacheFileContent = "\n";
+        foreach ($writtenEnvVars as $name => $value) {
+            $cacheFileContent .= "putenv('$name=$value');\n";
+            $cacheFileContent .= "\$_ENV['$name'] = '$value';\n";
+            $cacheFileContent .= "\$_SERVER['$name'] = '$value';\n";
+        }
+        return $cacheFileContent;
+    }
 }
